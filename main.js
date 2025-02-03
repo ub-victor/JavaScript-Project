75ddbffbb9a6016df1193b16b26ecab14ba3b5a8
ub-victor <victoireushindi371@gmail.com>
2025-02-04 00:12:48 +0200

    add a comment to the fourth line

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function calc(){
     var a = parseInt(document.querySelector('#value1').value) // Get the value from the input with ID 'value1' and convert it to an integer
     var b = parseInt(document.querySelector('#value2').value)
-    var op = document.querySelector('#operator').value
+    var op = document.querySelector('#operator').value // Get the selected operator from the dropdown for calculation
     var calculate;
 
     if(op == "add"){
