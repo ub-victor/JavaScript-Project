d7a273271f7bc0a852f3b4d24909d85d1cd331d4
ub-victor <victoireushindi371@gmail.com>
2025-02-03 18:17:06 +0200

    work on main.js code to start retrieving element with .value inside of parseInt to covert the string representation of a number (which you get from the .value property) into an integer.

---
+++ main.js
@@ -0,0 +1,7 @@
+function calc(){
+    var a = parseInt(document.querySelector('#value1').value) // Get the value from the input with ID 'value1' and convert it to an integer
+    var b = parseInt(document.querySelector('#value2').value)
+    var op = parseInt(document.querySelector('#operator').value)
+    var calculate;//10:35
+    
+}
