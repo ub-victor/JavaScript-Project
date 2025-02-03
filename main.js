8dec80e01d059c954755e84b3721cfe360c2a50a
ub-victor <victoireushindi371@gmail.com>
2025-02-03 22:25:40 +0200

    make condootion

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ function calc(){
     var a = parseInt(document.querySelector('#value1').value) // Get the value from the input with ID 'value1' and convert it to an integer
     var b = parseInt(document.querySelector('#value2').value)
     var op = parseInt(document.querySelector('#operator').value)
-    var calculate;//10:35
+    var calculate;
+
+    if(op == "add"){
+        calculate = a + b;
+    }else if (op == 'min'){
+        calculate = a - b;
+    }else if (op == 'div'){
+        calculate = a / b;
+    }else if (op == 'mul'){
+        calculate = a * b;
+    }
+
+
+    console.log(calculate)
     
 }
