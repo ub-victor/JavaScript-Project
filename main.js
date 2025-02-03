d21cd48e0183071eba1aff245f0688174d9f034a
ub-victor <victoireushindi371@gmail.com>
2025-02-03 22:42:54 +0200

    document.querySelector('#result').innerHTML = calculate;

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function calc(){
     var a = parseInt(document.querySelector('#value1').value) // Get the value from the input with ID 'value1' and convert it to an integer
     var b = parseInt(document.querySelector('#value2').value)
-    var op = parseInt(document.querySelector('#operator').value)
+    var op = document.querySelector('#operator').value
     var calculate;
 
     if(op == "add"){
@@ -15,6 +15,6 @@ function calc(){
     }
 
 
-    console.log(calculate)
+    document.querySelector('#result').innerHTML = calculate;
     
 }
