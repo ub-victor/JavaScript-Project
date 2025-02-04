3495cba975178fc8d9e9d18e8b930ff4f79cc8a4
ub-victor <victoireushindi371@gmail.com>
2025-02-05 00:00:17 +0200

    The '+' converts the input string to a number

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function calc(){
-    var a = parseInt(document.querySelector('#value1').value) // Get the value from the input with ID 'value1' and convert it to an integer
-    var b = parseInt(document.querySelector('#value2').value)
-    var op = document.querySelector('#operator').value // Get the selected operator from the dropdown for calculation
+    var a = +(document.querySelector('#value1').value) // The '+' converts the input string to a number 
+    var b = +(document.querySelector('#value2').value)
+    var op = document.querySelector('#operator').value // Or use parseInt it help Get the selected operator from the dropdown for calculation
     var calculate;
 
     if(op == "add"){
